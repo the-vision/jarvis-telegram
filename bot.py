24e44eedaf8ade81c01ff626498e71a5d656f132
Swapnil Agarwal <hi+github@swapnil.net>
2020-02-17 01:54:20 +0530

    Integrate with Postgres for analytics

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 from flask import Flask, request
 
@@ -9,15 +10,25 @@ TOKEN = os.environ.get('TELEGRAM_BOT_API_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
+DB = os.environ.get('DATABASE_URL')
+conn = psycopg2.connect(DB, sslmode='require')
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    log('start', None, message.text, message.from_user.id, None)
     bot.reply_to(message, 'At your service, ' + message.from_user.first_name + '!')
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+
+def log(intent, entities, input, sender, postback):
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO logs (intent, entities, input, sender, postback) VALUES (%s, %s, %s, %s, %s)", (intent, entities, input, sender, postback));
+        conn.commit()
 
 
 @server.route('/' + TOKEN, methods=['POST'])
