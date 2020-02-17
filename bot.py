c8611feb8407cd6bc681ba7830648414714fe5fb
Swapnil Agarwal <hi+github@swapnil.net>
2020-02-18 02:30:28 +0530

    Integrate with Mind Stone for processing

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
-import os
-import psycopg2
-
 from flask import Flask, request
 
+import json
+import os
+import psycopg2
+import requests
 import telebot
 
-URL = os.environ.get('HEROKU_PROJECT_URL')
-TOKEN = os.environ.get('TELEGRAM_BOT_API_TOKEN')
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
-
+CONFIDENCE_THRESHOLD = float(os.environ.get('CONFIDENCE_THRESHOLD'))
+FALLBACK_INTENT = os.environ.get('FALLBACK_INTENT')
 DB = os.environ.get('DATABASE_URL')
+MIND_STONE = os.environ.get('MIND_STONE')
+TOKEN = os.environ.get('TELEGRAM_BOT_API_TOKEN')
+URL = os.environ.get('HEROKU_PROJECT_URL')
+
+
+bot = telebot.TeleBot(TOKEN)
 conn = psycopg2.connect(DB, sslmode='require')
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -21,8 +26,27 @@ def start(message):
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
+def process_query(message):
+    response = requests.get(MIND_STONE + '/parse?q=' + message.text)
+    data = extract_structured_data(response.json())
+    log(data['intent'], json.dumps(data['entities']), message.text, message.from_user.id, None)
     bot.reply_to(message, message.text)
+
+
+def extract_structured_data(result):
+    data = {
+        'intent': FALLBACK_INTENT,
+        'entities': []
+    }
+    if result['intent']['confidence'] > CONFIDENCE_THRESHOLD:
+        data['intent'] = result['intent']['name']
+    for entity in result['entities']:
+        if entity['confidence'] > CONFIDENCE_THRESHOLD:
+            data['entities'].append({
+                'name': entity['entity'],
+                'value': entity['value']
+            })
+    return data
 
 
 def log(intent, entities, input, sender, postback):
