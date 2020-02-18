109ad0953d06457414d86fc7b2f48149d96b0261
Swapnil Agarwal <hi+github@swapnil.net>
2020-02-19 03:53:25 +0530

    Add xkcd module

--- modules.py
+++ modules.py
@@ -1,2 +1,12 @@
+import xkcd
+
+
 def reply(bot, message, intent, entities):
-    bot.reply_to(message, message.text)
+    if intent == 'xkcd':
+        random_comic = xkcd.getRandomComic()
+        bot.send_photo(message.chat.id, random_comic.getImageLink(), caption='*' +
+                       random_comic.getTitle() + '*\n' + random_comic.getAltText() +
+                       '\n' + random_comic.getExplanation(),
+                       parse_mode='Markdown', reply_to_message_id=message.message_id)
+    else:
+        bot.reply_to(message, message.text)
