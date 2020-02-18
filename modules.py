1fdf4b37dd2fa4b01962ffc5f300b79d447c7006
Swapnil Agarwal <hi+github@swapnil.net>
2020-02-19 04:59:05 +0530

    Add check out another xkcd option

--- modules.py
+++ modules.py
@@ -6,11 +6,11 @@ import xkcd
 def reply(bot, message, intent, entities):
     if intent == 'xkcd':
         random_comic = xkcd.getRandomComic()
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('See Explanation', url=random_comic.getExplanation()))
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton('Check out another xkcd comic!'))
         bot.send_photo(message.chat.id, random_comic.getImageLink(), caption='*' +
-                       random_comic.getTitle() + '*\n' + random_comic.getAltText(),
-                       parse_mode='Markdown', reply_to_message_id=message.message_id,
-                       reply_markup=markup)
+                       random_comic.getTitle() + '*\n' + random_comic.getAltText() +
+                       '\n' + random_comic.getExplanation(), parse_mode='Markdown',
+                       reply_to_message_id=message.message_id, reply_markup=markup)
     else:
         bot.reply_to(message, message.text)
