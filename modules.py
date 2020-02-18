75c3e38d8542b004d8a59ba8363eb91784b9a2ed
Swapnil Agarwal <hi+github@swapnil.net>
2020-02-19 04:22:58 +0530

    Add explanation link button

--- modules.py
+++ modules.py
@@ -1,12 +1,16 @@
+from telebot import types
+
 import xkcd
 
 
 def reply(bot, message, intent, entities):
     if intent == 'xkcd':
         random_comic = xkcd.getRandomComic()
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('See Explanation', url=random_comic.getExplanation()))
         bot.send_photo(message.chat.id, random_comic.getImageLink(), caption='*' +
-                       random_comic.getTitle() + '*\n' + random_comic.getAltText() +
-                       '\n' + random_comic.getExplanation(),
-                       parse_mode='Markdown', reply_to_message_id=message.message_id)
+                       random_comic.getTitle() + '*\n' + random_comic.getAltText(),
+                       parse_mode='Markdown', reply_to_message_id=message.message_id,
+                       reply_markup=markup)
     else:
         bot.reply_to(message, message.text)
