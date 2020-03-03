d3cfdbbd94c2c2d0eb0b484778b589c8d8b4458c
vvijayalakshmi21 <34595292+vvijayalakshmi21@users.noreply.github.com>
2020-03-03 01:43:57 -0600

    Add new intent : help
    Fix for issue #8

--- modules.py
+++ modules.py
@@ -12,5 +12,15 @@ def reply(bot, message, intent, entities):
                        random_comic.getTitle() + '*\n' + random_comic.getAltText() +
                        '\n' + random_comic.getExplanation(), parse_mode='Markdown',
                        reply_to_message_id=message.message_id, reply_markup=markup)
+    elif message.text == 'help':
+        help_message = """Hi there! I'm Jarvis, your personal assistant.\nYou can tell me things like:
+                        \n - trending news
+                        \n - motivate me
+                        \n - whats the weather like in Chennai?
+                        \n - gold price
+                        \n - show a random xkcd comic
+                        \n - make me laugh
+                        \nI'm always learning, so do come back and say hi from time to time!\nHave a nice day. 🙂"""
+        bot.reply_to(message, (help_message))
     else:
         bot.reply_to(message, message.text)
