2fb7f9f3ac4e4b0600bc3019f951ef326e31efb3
Anant Verma <vermaanant25@gmail.com>
2020-03-05 06:40:44 +0530

    add facts module

--- modules.py
+++ modules.py
@@ -1,7 +1,7 @@
 from telebot import types
 
 import xkcd
-
+import requests
 
 def reply(bot, message, intent, entities):
     if intent == 'xkcd':
@@ -12,5 +12,11 @@ def reply(bot, message, intent, entities):
                        random_comic.getTitle() + '*\n' + random_comic.getAltText() +
                        '\n' + random_comic.getExplanation(), parse_mode='Markdown',
                        reply_to_message_id=message.message_id, reply_markup=markup)
+    elif intent.lower() == 'fact':
+        response = requests.get('http://numbersapi.com/random/trivia')
+        if(response.status_code==200):
+            bot.reply_to(message, response.content)
+        else:
+            bot.reply_to(message, 'Something went wrong. Try again')
     else:
         bot.reply_to(message, message.text)
