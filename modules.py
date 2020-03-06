837aa5bd449bd24d33a39e2ac4ee808b4e100b02
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-03-06 15:58:22 +0530

    Add hello module (#25)
    Close #7 
    
    * hello intent added
    
    * Update settings.json
    
    * Update modules.py
    
    Co-authored-by: Swapnil Agarwal <2833845+swapagarwal@users.noreply.github.com>

--- modules.py
+++ modules.py
@@ -1,5 +1,6 @@
 from telebot import types
 
+import random
 import xkcd
 
 
@@ -12,5 +13,14 @@ def reply(bot, message, intent, entities):
                        random_comic.getTitle() + '*\n' + random_comic.getAltText() +
                        '\n' + random_comic.getExplanation(), parse_mode='Markdown',
                        reply_to_message_id=message.message_id, reply_markup=markup)
+    elif intent == 'hello':
+        greetings = [
+            'Hello there!',
+            'Hey!',
+            'Hi!',
+            'Oh hello!'
+        ]
+        greeting = random.choice(greetings)
+        bot.reply_to(message, greeting)                   
     else:
         bot.reply_to(message, message.text)
