ffe90ab3fda19e82fa4a4e7930b17b52bebdc692
Anant Verma <vermaanant25@gmail.com>
2020-03-07 17:36:53 +0530

    add apod module

--- bot.py
+++ bot.py
@@ -18,6 +18,8 @@ bot = telebot.TeleBot(TOKEN)
 conn = psycopg2.connect(DB, sslmode='require')
 server = Flask(__name__)
 
+NASA_API_KEY = os.environ.get('NASA_API_KEY')
+nasa_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(NASA_API_KEY)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -37,6 +39,26 @@ def process_query(message):
     log(conn, intent, json.dumps(entities), message.text, message.from_user.id, None)
     reply(bot, message, intent, entities)
 
+@bot.callback_query_handler(func=lambda call: True)
+def test_callback(call):
+    if(call.data=='explanation'):
+        response = requests.get(nasa_url)
+        if(response.status_code==200):
+            bot.send_message(call.message.chat.id, 'Explanation: {}'.format(response.json()['explanation']))
+        else:
+            bot.send_message(call.message.chat.id, 'Something went wrong. Try again.')
+    elif(call.data=='date'):
+        response = requests.get(nasa_url)
+        if(response.status_code==200):
+            bot.send_message(call.message.chat.id, 'Date: {}'.format(response.json()['date']))
+        else:
+            bot.send_message(call.message.chat.id, 'Something went wrong. Try again.')
+    elif(call.data=='copyright'):
+        response = requests.get(nasa_url)
+        if(response.status_code==200):
+            bot.send_message(call.message.chat.id, 'Copyright: {}'.format(response.json()['copyright']))
+        else:
+            bot.send_message(call.message.chat.id, 'Something went wrong. Try again.')
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
--- modules.py
+++ modules.py
@@ -2,7 +2,9 @@ from telebot import types
 
 import xkcd
 import requests
-
+import os
+NASA_API_KEY = os.environ.get('NASA_API_KEY')
+nasa_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(NASA_API_KEY)
 def reply(bot, message, intent, entities):
     if intent == 'xkcd':
         random_comic = xkcd.getRandomComic()
@@ -18,5 +20,16 @@ def reply(bot, message, intent, entities):
             bot.reply_to(message, response.content)
         else:
             bot.reply_to(message, 'Something went wrong. Try again')
+    elif intent.lower() == 'apod':
+        response = requests.get(nasa_url)
+        if(response.status_code==200):
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton('Explanation', callback_data='explanation'))
+            markup.add(types.InlineKeyboardButton('Date', callback_data='date'))
+            markup.add(types.InlineKeyboardButton('Copyright', callback_data='copyright'))
+            bot.send_photo(message.chat.id, response.json()['url'], caption= response.json()['title'],reply_to_message_id=message.message_id, reply_markup = markup)
+        else:
+            
+            bot.reply_to(message, 'Something went wrong. Try again!')
     else:
         bot.reply_to(message, message.text)
