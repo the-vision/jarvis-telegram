98e88617d198e035b4c10dcdd7d9a97d0ea39299
divya-prakash-1 <medivyaprakash@gmail.com>
2020-03-25 08:37:45 +0530

    bye:send another gif code added
    another gif is added and selected one out of two to send randomly.

--- modules.py
+++ modules.py
@@ -34,7 +34,8 @@ def reply(bot, message, intent, entities):
         ]
         greeting = random.choice(greetings)
         bot.reply_to(message, greeting)
-        bot.send_animation(message.chat.id, 'https://media.giphy.com/media/UrcXN0zTfzTPi/giphy.gif')
+        ani = random.choice(['https://media.giphy.com/media/UrcXN0zTfzTPi/giphy.gif','https://media.giphy.com/media/3o6EhGvKschtbrRjX2/giphy.gif'])
+        bot.send_animation(message.chat.id, ani)
     elif intent == 'coin':
         coin_images = {
             'Heads': 'https://www.ssaurel.com/blog/wp-content/uploads/2017/01/heads.png',
